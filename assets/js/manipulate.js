14b41a126c14f96c3beb98d188083e1c03c908fd
Joseph Cassell <yosefcassell@gmail.com>
2023-09-27 17:21:07 -0700

    done

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,19 @@
-function changeTitle() {
+export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
-
+   // const title = document.getElementById('title');
+      window.document.title = "Window Object!"
     // Your code here
 }
 
-function changeHeader() {
+export function changeHeader() {
     // Change the name in the h1 of the page to your name
-
+    document.querySelector('h1').innerText = "Carmen!"
     // Your code here
 }
 
-function changeAboutMe() {
+export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
-
+     document.querySelector('.section p').innerText = "Carmen is a good coder!"
     // Your code here
 }
