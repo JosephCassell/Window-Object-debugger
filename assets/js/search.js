eeb08165a93fbf00111dd808eb960ae611d511ed
Carmen <carmenshiu07@gmail.com>
2023-09-27 17:48:27 -0700

    finished all

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,24 +6,24 @@ export function findElementById(id) {
 
 export function findFirstElementOfTag(tag) {
     // Return the first occurence of an element of tag name `tag`
-    return document.querySelector(`${tag}`)
+    return document.querySelector(tag)
     // Your code here
 }
 
 export function findFirstElementOfClass(cls) {
     // Return the first occurence of an element of class `cls`
-
+    return document.getElementsByClassName(cls)[0]
     // Your code here
 }
 
 export function findElementsOfTag(tag) {
     // Return an array of elements that have a tag name of `tag`
-
+    return document.querySelectorAll(tag)
     // Your code here
 }
 
 export function findElementsOfClass(cls) {
     // Return an array of elements that have are of class `cls`
-
+    return document.getElementsByClassName(cls)
     // Your code here
 }
